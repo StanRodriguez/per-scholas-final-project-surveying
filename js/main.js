2945d8d528292b64e979e5451400aa86ccacdb32
Student_06 <Student_06@PSXMA106.local>
2018-11-21 10:17:17 -0500

    Adding files

---
+++ js/main.js
@@ -0,0 +1,261 @@
+
+
+
+// let slide = {
+// 	folder: '',
+// 	image: [],
+// 	current: 0,
+// 	slides: [1],
+// 	setValues() {
+// 		this.folder = './images/slideshow/';
+// 		this.image = ['1.jpg','2.jpg','3.jpg','4.jpg'];
+// 		this.current = 0;
+// 		this.slides = document.getElementsByClassName('slides');
+// 	},
+// 	makeSlide(index) {
+// 		// body...
+// 		slide.setValues();
+// 		console.log(this.current)
+// 		this.current += index;
+// 		//start from 0;
+// 		if (this.current > slide.image.length-1) {
+// 			this.current = 0;
+// 		}
+// 		//start from last
+// 		if (this.current < 0) {
+// 			this.current = this.image.length -1;
+// 		}
+
+// 		//make everything invicible
+// 		for (var i = 0; i < this.slides.length; i++) {
+// 			this.slides[i].style.display='none'
+// 		}
+
+// 		this.slides[this.current].style.display = 'block'
+// 	},
+// 	freeze() {
+//  		clearInterval(interval)
+// 	},
+// 	start() {
+//  		interval = setInterval(this.makeSlide, 3000,1)
+// 	}
+// }
+// let interval=setInterval(this.makeSlide, 3000,1);
+
+let folder = './images/slideshow/';
+let images =['1.jpg','2.jpg','3.jpg','4.jpg'];
+let current = 0;
+
+const slide = (index) =>{
+
+	let slides = document.getElementsByClassName('slides')
+	current += index;
+	//start from 0;
+	if (current > images.length-1) {
+		current = 0;
+	}
+	//start from last
+	if (current < 0) {
+		current = images.length -1;
+	}
+
+	//make everything invicible
+	for (var i = 0; i < slides.length; i++) {
+		slides[i].style.display='none'
+	}
+
+	slides[current].style.display = 'block'
+}
+let interval;
+//only run on index.html
+if (window.location.href.match('index.html') != null) {
+	interval=setInterval(slide, 3000,1);
+}
+
+//dont change the slideshow
+const freeze = ()=>{
+	clearInterval(interval)
+}
+
+//re start the slideshow
+const start = () =>{
+	interval = setInterval(slide, 3000,1);
+}
+
+//send mail
+let emailto = {
+	fname: '',
+	lname: '',
+	phone: '',
+	subject: '',
+	email: 'dssdominicana@gmail.com',
+	message: '',
+	mailto: '',
+	setEmail: function(){
+		
+			// this.email = document.getElementById('contact-email').value;
+			this.subject = document.getElementById('contact-subject').value;
+			this.message = document.getElementById('contact-message').value;
+			this.fname = document.getElementById('contact-fname').value;
+			this.lname = document.getElementById('contact-lname').value;
+			this.phone = document.getElementById('contact-phone').value;
+			this.subject = encodeURI(document.getElementById('contact-subject').value);
+			// this.email = encodeURI(document.getElementById('contact-email').value);
+			this.message = encodeURI(document.getElementById('contact-message').value);
+			this.mailto = `mailto:${this.email}?subject=${this.subject}&body=${this.message}`;
+	
+	},
+	sendEmail: function () {
+		// body...
+		this.setEmail();
+		if (this.email == '') {
+			document.getElementById('contact-email').style.border = '5px solid red';
+			document.getElementById('contact-email').focus();
+		}else if (this.subject == '') {
+			document.getElementById('contact-subject').style.border = '5px solid red';
+			document.getElementById('contact-subject').focus();
+		}else if (this.message == '') {
+			document.getElementById('contact-message').style.border = '5px solid red';
+			document.getElementById('contact-message').focus();
+		}else{
+			window.open(this.mailto);
+		}
+	}
+}
+
+const appearArrow = ()=>{
+
+if (document.body.scrollTop < 350 || document.documentElement.scrollTop < 350) {
+
+		document.getElementById('arrow').className = "arrow";
+
+    }
+
+	if (document.body.scrollTop > 350 || document.documentElement.scrollTop > 350) {
+		document.getElementById('arrow').className = "appear-arrow";
+    }
+}
+//appear sections
+const appear = ()=>{
+	if (document.body.scrollTop > 350 || document.documentElement.scrollTop > 350) {
+		document.getElementById('service-two').className = "appear";
+    }
+
+
+    if (document.body.scrollTop > 800 || document.documentElement.scrollTop > 800) {
+		document.getElementById('service-three').className = "appear";
+
+    }
+    if (document.body.scrollTop > 1200 || document.documentElement.scrollTop > 1200) {
+		document.getElementById('service-four').className = "appear";
+
+    }
+}
+
+
+
+// $( "#news" ).load( "../pages/contact.html", function() {
+//   alert( "Load was performed." );
+// });
+
+
+
+class News {
+
+	constructor(){
+		this.headers =['Nuevo Logo empresarial','Nuevo deslinde'];
+		this.images =['../images/logo.png','../images/logo.png'];
+		this.descriptions = [
+			'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel nobis voluptates officia cumque nulla consequatur in quia, amet doloribus fugiat, molestiae, minima perferendis saepe? Aliquid itaque excepturi cumque neque odit!',
+			'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut magni nulla recusandae tenetur magnam officiis, distinctio adipisci fugiat rem facere, necessitatibus nemo voluptas sint aspernatur a asperiores, ducimus eaque laboriosam!'
+		]
+		this.wholeDescriptions = ["Stanly",
+			"Nando"
+		]
+	}
+	createNews(){
+		for (let i = 0; i < this.headers.length; i++) {
+			let h1 = document.createElement("h1");
+			let h1Content = document.createTextNode(this.headers[i]);
+			h1.appendChild(h1Content);
+
+			let img = document.createElement("img");
+			img.src = this.images[i];
+
+			let p = document.createElement("p");
+			let pContent = document.createTextNode(this.descriptions[i]);
+			p.appendChild(pContent);
+
+
+			let button = document.createElement("button");
+			let ButtonContent = document.createTextNode("Leer mas");
+			button.appendChild(ButtonContent);
+
+			let a = document.createElement("a");
+			a.href = `new.html?new=${i}`;
+
+			a.appendChild(button);
+			// button.value="Read More";
+
+			document.getElementById('news').appendChild(h1);
+			document.getElementById('news').appendChild(img);
+			document.getElementById('news').appendChild(p);
+			document.getElementById('news').appendChild(a);
+		}
+
+	}
+	createNew(index){
+		let h1 = document.createElement("h1");
+			let h1Content = document.createTextNode(this.headers[index]);
+			h1.appendChild(h1Content);
+
+			let img = document.createElement("img");
+			img.src = this.images[index];
+
+			let p = document.createElement("p");
+			let pContent = document.createTextNode(this.wholeDescriptions[index]);
+			p.appendChild(pContent);
+
+
+			// let button = document.createElement("button");
+			// let ButtonContent = document.createTextNode("Read More");
+			// button.appendChild(ButtonContent);
+
+			// let a = document.createElement("a");
+			// a.href = "new.html";
+
+			// a.appendChild(button);
+			// button.value="Read More";
+
+			document.getElementById('new').appendChild(h1);
+			document.getElementById('new').appendChild(img);
+			document.getElementById('new').appendChild(p);
+			// document.getElementById('new').appendChild(a);
+
+	}
+}
+
+//control which code is going to run in each page
+if (window.location.href.match('services.html') != null) {
+		window.onscroll = function () {
+		appearArrow();
+		appear();
+	}
+}else if (window.location.href.match('news.html') != null) {
+	let news = new News(); 
+	news.createNews();
+	window.onscroll = function () {
+		appearArrow();
+	}
+}else if (window.location.href.match('us.html') != null) {
+	window.onscroll = function () {
+		appearArrow();
+	}
+}else if (window.location.href.match('new.html') != null) {
+	let index = window.location.href[window.location.href.length-1];
+	let news = new News(); 
+	news.createNew(index);
+	window.onscroll = function () {
+		appearArrow();
+	}
+}
